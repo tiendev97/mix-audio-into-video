84c4cb4b5b4011340d81fd6ac02f6907df835aab
tiendev97 <tien.dev97@gmail.com>
2022-04-03 07:32:27 +0700

    Refactor ExportService

--- app/src/main/java/com/tienducky/mixaudioandvideo/export/ExportService.java
+++ app/src/main/java/com/tienducky/mixaudioandvideo/export/ExportService.java
@@ -21,14 +21,28 @@ import java.nio.ByteBuffer;
 
 public class ExportService {
     private static final String TAG = "ExportService";
-    private BackgroundThreadPoster mExportThread = new BackgroundThreadPoster();
-    private UiThreadPoster mUIThread = new UiThreadPoster();
+    private final BackgroundThreadPoster mExportThread = new BackgroundThreadPoster();
+    private final UiThreadPoster mUIThread = new UiThreadPoster();
     private ExportElement mExportElement;
     private ExportAdapter mExportAdapter;
     private File mOutputFile;
 
     // export params
-    private long mTotalDuration;
+    private long mVideoDuration;
+    private long mExportDuration;
+    private long mLastVideoTimeUs;
+    private MediaExtractor mVideoExtractor;
+    private MediaExtractor mAudioExtractor;
+    private MediaFormat mInputVideoFormat;
+    private MediaFormat mInputAudioFormat;
+    private MediaCodec mAudioDecoder;
+    private MediaCodec mAudioEncoder;
+    private MediaMuxer muxer;
+    private int mInputVideoTrack;
+    private int mInputAudioTrack;
+    private int mMuxerVideoTrack;
+    private int mMuxerAudioTrack;
+
 
     public void startExport(File outputFile, ExportElement exportElement, ExportAdapter exportAdapter) {
         mExportElement = exportElement;
@@ -39,129 +53,208 @@ public class ExportService {
 
     private void exportVideo() {
         Log.i(TAG, "exportVideo " + mExportElement.getAudioFilePath() + ", " + mExportElement.getVideoFilePath() + ", " + mOutputFile.getPath());
-        MediaExtractor videoExtractor = new MediaExtractor();
-        MediaExtractor audioExtractor = new MediaExtractor();
-        MediaMuxer muxer = null;
+
         try {
-            videoExtractor = new MediaExtractor();
-            videoExtractor.setDataSource(mExportElement.getVideoFilePath());
-            audioExtractor = new MediaExtractor();
-            audioExtractor.setDataSource(mExportElement.getAudioFilePath());
-
-            int inputVideoTrack = MediaUtils.getTrackIndex(videoExtractor, TrackType.VIDEO);
-            int inputAudioTrack = MediaUtils.getTrackIndex(audioExtractor, TrackType.AUDIO);
-
-            // export video format
-            MediaFormat inputVideoFormat = videoExtractor.getTrackFormat(inputVideoTrack);
-            MediaFormat inputAudioFormat = audioExtractor.getTrackFormat(inputAudioTrack);
-
-            long videoDuration = inputVideoFormat.getLong(MediaFormat.KEY_DURATION);
-            mTotalDuration  = videoDuration * 2;
-
-            Log.i(TAG, "inputVideoFormat = " + inputVideoFormat);
-            Log.i(TAG, "inputAudioFormat = " + inputAudioFormat);
-
-            muxer = new MediaMuxer(mOutputFile.getPath(), MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
-            int muxerVideoTrack = muxer.addTrack(inputVideoFormat);
-
-            long previousTime = System.currentTimeMillis();
-            mergeAudioIntoOutputFile(muxer, audioExtractor, inputAudioFormat, inputAudioTrack, videoDuration);
-            Log.i("tien.ngc", "time export audio " + (System.currentTimeMillis() - previousTime) / 60);
-            previousTime = System.currentTimeMillis();
-            mergeVideoIntoOutputFile(muxer, videoExtractor, inputVideoFormat, inputVideoTrack, muxerVideoTrack);
-            Log.i("tien.ngc", "time export video " + (System.currentTimeMillis() - previousTime));
-
-            muxer.stop();
-            mUIThread.post(() -> mExportAdapter.onExportComplete());
+            initResources();
+            startExportVideo();
+            startExportAudio();
+            handleExportComplete();
         } catch (Exception ex) {
-            Log.d(TAG, ex.toString());
-            ex.printStackTrace();
-            mOutputFile.delete();
-            mUIThread.post(() -> mExportAdapter.onExportError());
+            handleExportFailed(ex);
         } finally {
-            if (muxer != null)
-                muxer.release();
-            videoExtractor.release();
-            audioExtractor.release();
+            cleanup();
         }
     }
 
     /***
-     * This method is used to merge to output video file
-     * @param muxer
-     * @param videoExtractor
-     * @param inputVideoFormat
-     * @param inputVideoTrack
+     * init resources for exporting
+     *
+     * @throws IOException
+     */
+    private void initResources() throws IOException {
+        // create audio and video extractor
+        mVideoExtractor = createMediaExtractor(mExportElement.getVideoFilePath());
+        mAudioExtractor = createMediaExtractor(mExportElement.getAudioFilePath());
+
+        // get audio and video track
+        mInputVideoTrack = MediaUtils.getTrackIndex(mVideoExtractor, TrackType.VIDEO);
+        mInputAudioTrack = MediaUtils.getTrackIndex(mAudioExtractor, TrackType.AUDIO);
+
+        // export video format
+        mInputVideoFormat = mVideoExtractor.getTrackFormat(mInputVideoTrack);
+        mInputAudioFormat = mAudioExtractor.getTrackFormat(mInputAudioTrack);
+
+        // calculate export video duration
+        mVideoDuration = mInputVideoFormat.getLong(MediaFormat.KEY_DURATION);
+        mExportDuration = mVideoDuration * 2;
+
+        // prepare audio decoder and encoder
+        prepareAudioDecoderAndEncoder();
+
+        // prepare media muxer
+        prepareMediaMuxer();
+    }
+
+    /***
+     * handle export complete
+     */
+    private void handleExportComplete() {
+        mUIThread.post(() -> mExportAdapter.onExportComplete());
+    }
+
+    /***
+     * cleanup resources
+     */
+    private void cleanup() {
+        if (muxer != null) {
+            muxer.stop();
+            muxer.release();
+            muxer = null;
+        }
+
+        if(mAudioDecoder != null) {
+            mAudioDecoder.stop();
+            mAudioDecoder.release();
+            mAudioDecoder = null;
+        }
+
+        if(mAudioEncoder != null) {
+            mAudioEncoder.stop();
+            mAudioEncoder.release();
+            mAudioEncoder = null;
+        }
+
+        if(mVideoExtractor != null) {
+            mVideoExtractor.release();
+            mVideoExtractor = null;
+        }
+
+        if(mAudioExtractor != null) {
+            mAudioExtractor.release();
+            mVideoExtractor = null;
+        }
+    }
+
+    /***
+     * Handle export complete
+     *
+     * @param ex
+     */
+    private void handleExportFailed(Exception ex) {
+        Log.d(TAG, ex.toString());
+        ex.printStackTrace();
+        mOutputFile.delete();
+        mUIThread.post(() -> mExportAdapter.onExportError());
+    }
+
+    /***
+     * create audio decoder and encoder
+     *
+     * @throws IOException
+     */
+    private void prepareAudioDecoderAndEncoder() throws IOException {
+        Log.i(TAG,"1. prepareAudioDecoderAndEncoder ");
+        mAudioExtractor.selectTrack(mInputAudioTrack);
+        mAudioDecoder = MediaCodec.createDecoderByType(mInputAudioFormat.getString(MediaFormat.KEY_MIME));
+        mAudioDecoder.configure(mInputAudioFormat, null, null, 0);
+        mAudioDecoder.start();
+
+        MediaFormat audioOutputFormat = prepareAudioOutputFormat(mInputAudioFormat);
+        mAudioEncoder = MediaCodec.createEncoderByType(audioOutputFormat.getString(MediaFormat.KEY_MIME));
+        mAudioEncoder.configure(audioOutputFormat, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
+        mAudioEncoder.start();
+    }
+
+    /***
+     * prepare media muxer
+     *
+     * @throws IOException
+     */
+    private void prepareMediaMuxer() throws IOException {
+        Log.i(TAG,"2. prepareMediaMuxer ");
+        muxer = new MediaMuxer(mOutputFile.getPath(), MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
+        mMuxerVideoTrack = muxer.addTrack(mInputVideoFormat);
+        mMuxerAudioTrack = muxer.addTrack(mAudioEncoder.getOutputFormat());
+        muxer.start();
+        Log.i(TAG, "2. muxerVideoTrack " + mMuxerVideoTrack);
+        Log.i(TAG, "2. muxerAudioTrack " + mMuxerAudioTrack);
+    }
+
+    /***
+     * create new MediaExtractor from file path
+     *
+     * @param filePath
+     * @return
+     * @throws IOException
+     */
+    private MediaExtractor createMediaExtractor(String filePath) throws IOException {
+        MediaExtractor extractor = new MediaExtractor();
+        extractor.setDataSource(filePath);
+        return extractor;
+    }
+
+    /***
+     * start export video
      */
     @SuppressLint("WrongConstant")
-    private void mergeVideoIntoOutputFile(MediaMuxer muxer, MediaExtractor videoExtractor, MediaFormat inputVideoFormat, int inputVideoTrack, int muxerVideoTrack) {
-        videoExtractor.selectTrack(inputVideoTrack);
+    private void startExportVideo() {
+        long startTime = System.currentTimeMillis();
+        int maxBufferSize = mInputVideoFormat.getInteger(MediaFormat.KEY_MAX_INPUT_SIZE);
         MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
-        int maxBufferSize = inputVideoFormat.getInteger(MediaFormat.KEY_MAX_INPUT_SIZE);
         ByteBuffer videoBuffer = ByteBuffer.allocateDirect(maxBufferSize);
-//        long lastVideoTimeUs = 0;
+        mVideoExtractor.selectTrack(mInputVideoTrack);
+
         while (true) {
-            int size = videoExtractor.readSampleData(videoBuffer, 0);
-            if (size == -1) {
+            int size = mVideoExtractor.readSampleData(videoBuffer, 0);
+            if (size == -1) { // last file
                 break;
             }
-            long sampleTimeUs = videoExtractor.getSampleTime();
-            int flags = videoExtractor.getSampleFlags();
+            long sampleTimeUs = mVideoExtractor.getSampleTime();
+            int flags = mVideoExtractor.getSampleFlags();
             bufferInfo.presentationTimeUs = sampleTimeUs;
             bufferInfo.flags = flags;
             bufferInfo.size = size;
-            muxer.writeSampleData(muxerVideoTrack, videoBuffer, bufferInfo);
-//            lastVideoTimeUs = sampleTimeUs;
-            videoExtractor.advance();
+            muxer.writeSampleData(mMuxerVideoTrack, videoBuffer, bufferInfo);
+            mLastVideoTimeUs = sampleTimeUs;
+            Log.i(TAG, "export video progress: { timeStamp " + sampleTimeUs + " , progress = " + getCurrentProgress(sampleTimeUs) + "}");
+            mVideoExtractor.advance();
         }
+
+        Log.i(TAG, "total time export video: " + (System.currentTimeMillis() - startTime));
+    }
+
+    private int getCurrentProgress(long timeStamp) {
+        return (int) ((float)timeStamp / mExportDuration * 100);
     }
 
     /***
-     * This method is used to merge audio into output video file
-     * @param muxer
-     * @param audioExtractor
-     * @param inputAudioFormat
-     * @param inputAudioTrack
-     * @throws IOException
+     * start export audio
      */
-    private void mergeAudioIntoOutputFile(MediaMuxer muxer, MediaExtractor audioExtractor, MediaFormat inputAudioFormat, int inputAudioTrack, long totalDuration) throws IOException {
-        audioExtractor.selectTrack(inputAudioTrack);
-        MediaCodec decoder = MediaCodec.createDecoderByType(inputAudioFormat.getString(MediaFormat.KEY_MIME));
-        decoder.configure(inputAudioFormat, null, null, 0);
-        decoder.start();
-
-        MediaFormat audioOutputFormat = prepareAudioOutputFormat(inputAudioFormat);
-        MediaCodec encoder = MediaCodec.createEncoderByType(audioOutputFormat.getString(MediaFormat.KEY_MIME));
-        encoder.configure(audioOutputFormat, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
-        encoder.start();
-
-        Log.i(TAG,"outputAudioFormat: " + audioOutputFormat);
-
+    private void startExportAudio() {
+        long startTime = System.currentTimeMillis();
         boolean allInputExtracted = false;
         boolean allInputDecoded = false;
         boolean allOutputEncoded = false;
-
         long timeoutUs = 10000L;
         MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
-        int muxerAudioTrack = -1;
 
         while (!allOutputEncoded) {
             // feed input to decoder
             if (!allInputExtracted) {
-                int inBufferId = decoder.dequeueInputBuffer(timeoutUs);
+                int inBufferId = mAudioDecoder.dequeueInputBuffer(timeoutUs);
                 if (inBufferId >= 0) {
-                    ByteBuffer buffer = decoder.getInputBuffer(inBufferId);
-                    int sampleSize = audioExtractor.readSampleData(buffer, 0);
+                    ByteBuffer buffer = mAudioDecoder.getInputBuffer(inBufferId);
+                    int sampleSize = mAudioExtractor.readSampleData(buffer, 0);
 
-                    if (sampleSize >= 0 && audioExtractor.getSampleTime() <= totalDuration) {
-                        decoder.queueInputBuffer(
+                    if (sampleSize >= 0 && mAudioExtractor.getSampleTime() <= mVideoDuration) {
+                        mAudioDecoder.queueInputBuffer(
                                 inBufferId, 0, sampleSize,
-                                audioExtractor.getSampleTime(),
-                                audioExtractor.getSampleFlags()
+                                mAudioExtractor.getSampleTime(),
+                                mAudioExtractor.getSampleFlags()
                         );
-                        audioExtractor.advance();
+                        mAudioExtractor.advance();
                     } else {
-                        decoder.queueInputBuffer(
+                        mAudioDecoder.queueInputBuffer(
                                 inBufferId, 0, 0,
                                 0, MediaCodec.BUFFER_FLAG_END_OF_STREAM
                         );
@@ -175,11 +268,12 @@ public class ExportService {
 
             while (encoderOutputAvailable || decoderOutputAvailable) {
                 // drain Encoder & mux first
-                int outBufferId = encoder.dequeueOutputBuffer(bufferInfo, timeoutUs);
+                int outBufferId = mAudioEncoder.dequeueOutputBuffer(bufferInfo, timeoutUs);
                 if (outBufferId >= 0) {
-                    ByteBuffer encodedBuffer = encoder.getOutputBuffer(outBufferId);
-                    muxer.writeSampleData(muxerAudioTrack, encodedBuffer, bufferInfo);
-                    encoder.releaseOutputBuffer(outBufferId, false);
+                    ByteBuffer encodedBuffer = mAudioEncoder.getOutputBuffer(outBufferId);
+                    muxer.writeSampleData(mMuxerAudioTrack, encodedBuffer, bufferInfo);
+                    mAudioEncoder.releaseOutputBuffer(outBufferId, false);
+                    Log.i(TAG, "export audio progress: { timeStamp = " + bufferInfo.presentationTimeUs + " , progress: " + getCurrentProgress(mLastVideoTimeUs + bufferInfo.presentationTimeUs) + "}");
 
                     // check finished
                     if ((bufferInfo.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0) {
@@ -188,9 +282,6 @@ public class ExportService {
                     }
                 } else if (outBufferId == MediaCodec.INFO_TRY_AGAIN_LATER) {
                     encoderOutputAvailable = false;
-                } else if (outBufferId == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
-                    muxerAudioTrack = muxer.addTrack(encoder.getOutputFormat());
-                    muxer.start();
                 }
 
                 if (outBufferId != MediaCodec.INFO_TRY_AGAIN_LATER)
@@ -198,33 +289,29 @@ public class ExportService {
 
                 // Get output from decoder and feed it to encoder
                 if (!allInputDecoded) {
-                    outBufferId = decoder.dequeueOutputBuffer(bufferInfo, timeoutUs);
+                    outBufferId = mAudioDecoder.dequeueOutputBuffer(bufferInfo, timeoutUs);
                     if (outBufferId >= 0) {
-                        ByteBuffer outBuffer = decoder.getOutputBuffer(outBufferId);
+                        ByteBuffer outBuffer = mAudioDecoder.getOutputBuffer(outBufferId);
 
                         // If needed, process decoded data here
                         // ...
-
                         // We drained the encoder, so there should be input buffer
                         // available. If this is not the case, we get a NullPointerException
                         // when touching inBuffer
-                        int inBufferId = encoder.dequeueInputBuffer(timeoutUs);
-                        ByteBuffer inBuffer = encoder.getInputBuffer(inBufferId);
+                        int inBufferId = mAudioEncoder.dequeueInputBuffer(timeoutUs);
+                        ByteBuffer inBuffer = mAudioEncoder.getInputBuffer(inBufferId);
 
                         // Copy buffers - decoder output goes to encoder input
                         inBuffer.put(outBuffer);
 
                         // Feed encoder
-                        encoder.queueInputBuffer(
+                        mAudioEncoder.queueInputBuffer(
                                 inBufferId, bufferInfo.offset,
                                 bufferInfo.size,
                                 bufferInfo.presentationTimeUs,
                                 bufferInfo.flags);
 
-                        Log.i(TAG, "audio presentationTimeUs " + bufferInfo.presentationTimeUs);
-
-                        decoder.releaseOutputBuffer(outBufferId, false);
-
+                        mAudioDecoder.releaseOutputBuffer(outBufferId, false);
                         // Did we get all output from decoder?
                         if ((bufferInfo.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0)
                             allInputDecoded = true;
@@ -235,16 +322,19 @@ public class ExportService {
                 }
             }
         }
+
+        Log.i(TAG, "total time export audio: " + (System.currentTimeMillis() - startTime));
     }
 
     /***
-     * This method is used to create new audio output format for export video
+     * create new audio output format
+     * Prepare output format aac/m4a
+     *
      * @param inputAudioFormat
      * @return
      */
     @NonNull
     private MediaFormat prepareAudioOutputFormat(MediaFormat inputAudioFormat) {
-        // Prepare output format for aac/m4a
         MediaFormat outputFormat = new MediaFormat();
         outputFormat.setString(MediaFormat.KEY_MIME, "audio/mp4a-latm");
         outputFormat.setInteger(MediaFormat.KEY_AAC_PROFILE, MediaCodecInfo.CodecProfileLevel.AACObjectLC);
